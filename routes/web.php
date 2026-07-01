70eb29bf8b0ce752d9049419f2f79e9ee1d47eb5
safriansyah <safriansyah077@gmail.com>
2026-07-01 09:19:34 +0700

    asdasda

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Send the root straight to the AutoPostSM admin panel.
+// Using redirect('admin') (no leading slash) keeps it correct when the app
+// is served from a sub-folder, e.g. https://domain/autopostsm/admin.
+Route::get('/', fn () => redirect('admin'));
